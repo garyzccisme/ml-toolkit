24c98a113faf6ce14a481aa33367160b6eb9946f
garyzcc <33635035+ChichengZhang@users.noreply.github.com>
2020-05-01 10:03:23 -0400

    focal loss

--- feedforward_neural_network/criterion.py
+++ feedforward_neural_network/criterion.py
@@ -28,6 +28,33 @@ class MultiClassLoss(nn.Module):
         return - log_likelihoods.mean()
 
 
+class MultiClassFocalLoss(nn.Module):
+    def __init__(self, gamma: int = 0, pre_log: bool = True):
+        """
+        This Module is designed as a customized loss criterion for multi-classification,
+        implementing - mean(log(likelihood)).
+        Args:
+            gamma: The parameter of focal factor.
+            pre_log: True if net_output has already been log transformed.
+        """
+        super().__init__()
+        self.gamma = gamma
+        self.pre_log = pre_log
+
+    def forward(self, net_output: torch.autograd.Variable, y: torch.Tensor) -> torch.autograd.Variable:
+        """
+        Args:
+            net_output: Predicted values, size should be [n, num_class], n is the number of records.
+            y: True values.
+        Returns: loss function variable
+        """
+        if self.pre_log:
+            log_likelihoods = net_output.gather(dim=1, index=y.type(torch.long).unsqueeze(1))
+        else:
+            log_likelihoods = net_output.log().gather(dim=1, index=y.type(torch.long).unsqueeze(1))
+        return - (log_likelihoods * (1 - log_likelihoods.exp()) ** self.gamma).mean()
+
+
 class NormalLoss(nn.Module):
     def __init__(self):
         """
