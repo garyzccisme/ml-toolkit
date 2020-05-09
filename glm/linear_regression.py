bf780694ba33b2e7bad2bc64832d8f3cd51c2501
garyzcc <33635035+ChichengZhang@users.noreply.github.com>
2020-05-08 22:06:27 -0400

    linear regression code & notebook

--- glm/linear_regression.py
+++ glm/linear_regression.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.linalg import inv
 
 
 class LinearRegression:
@@ -10,13 +11,13 @@ class LinearRegression:
         if len(X) != len(y):
             raise ValueError('Invalid input')
         if self.with_intercept:
-            X = np.stack((np.ones(len(X)), np.array(X)))
-        self.beta = (X.T @ X).I @ X.T @ y
+            X = np.concatenate((np.ones([len(X), 1]), np.array(X)), axis=1)
+        self.beta = inv(X.T @ X) @ X.T @ y
 
     def predict(self, X):
         X = np.array(X)
         if self.with_intercept:
-            X = np.stack((np.ones(len(X)), np.array(X)))
+            X = np.concatenate((np.ones([len(X), 1]), np.array(X)), axis=1)
         if X.shape[1] != len(self.beta):
             raise ValueError('Invalid input')
         y_pred = X @ self.beta
