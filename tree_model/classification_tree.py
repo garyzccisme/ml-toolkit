ac232228089818351e41e59b65686c6fc459b356
garyzcc <33635035+ChichengZhang@users.noreply.github.com>
2020-05-11 22:39:17 -0400

    refactor cart

--- tree_model/classification_tree.py
+++ tree_model/classification_tree.py
@@ -7,7 +7,7 @@ class Leaf:
 
 
 class Node:
-    def __init__(self, threshold, column_idx, left, right):
+    def __init__(self, threshold=None, column_idx=None, left=None, right=None):
         self.threshold = threshold
         self.column_idx = column_idx
         self.left = left
@@ -16,43 +16,53 @@ class Node:
 
 class ClassificationTree:
 
-    def __int__(self):
-        self.tree = None
+    def __init__(self):
+        self.tree = Node()
 
-    def fit(self, x, y):
-        self.tree = self.grow(x, y)
+    @staticmethod
+    def gini(y):
+        _, counts = np.unique(y, return_counts=True)
+        return 1 - ((counts / len(y)) ** 2).sum()
 
-    def grow(self, x, y):
+    def fit(self, X, y):
+        X = np.array(X)
+        y = np.array(y)
+        self.tree = self.grow(X, y)
 
+    def grow(self, X, y):
+
+        # Initialization
         best_gini = self.gini(y)
+        best_threshold = None
+        best_column_idx = None
+        left_x, left_y = None, None
+        right_x, right_y = None, None
+
         if best_gini == 0:
             return Leaf(label=y[0])
 
-        for column_idx in range(x.shape[1]):
-            for potential_threshold in np.unique(x[:, column_idx]):
+        # Search all possible split points in X
+        for column_idx in range(X.shape[1]):
+            for potential_threshold in np.unique(X[:, column_idx]):
 
                 # Note: np.where returns tuple of arrays
-                left_index = np.where(x[:, column_idx] <= potential_threshold)[0]
-                right_index = np.where(x[:, column_idx] > potential_threshold)[0]
+                left_index = np.nonzero(X[:, column_idx] <= potential_threshold)[0]
+                right_index = np.nonzero(X[:, column_idx] > potential_threshold)[0]
 
                 left_gini = self.gini(y[left_index])
                 right_gini = self.gini(y[right_index])
-                gini = len(left_index) / len(y) * left_gini + len(right_index) / len(y) * right_gini
+                gini = (len(left_index) * left_gini + len(right_index) * right_gini) / len(y)
 
                 if gini < best_gini:
                     best_gini = gini
                     best_column_idx, best_threshold = column_idx, potential_threshold
-                    left_x, left_y = x[left_index, :], y[left_index]
-                    right_x, right_y = x[right_index, :], y[right_index]
+                    left_x, left_y = X[left_index, :], y[left_index]
+                    right_x, right_y = X[right_index, :], y[right_index]
 
         root = Node(threshold=best_threshold, column_idx=best_column_idx,
                     left=self.grow(left_x, left_y), right=self.grow(right_x, right_y))
 
         return root
-
-    def gini(self, y):
-        _, counts = np.unique(y, return_counts=True)
-        return 1 - ((counts / len(y)) ** 2).sum()
 
     def predict(self, x, node=None):
 
